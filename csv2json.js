fe44a91d0b11dbd80f6ae032591d0dcedd29d983
Abhimanyu Singh <superhit0@gmail.com>
2020-05-06 21:36:19 +0530

    Using Worker to parse csv to JSON object

---
+++ csv2json.js
@@ -0,0 +1,33 @@
+function processRowToArray(row) {
+  let isSemiColon = false;
+  const tokens = row.split(',');
+  const result = [];
+  for (const token of tokens) {
+    if (isSemiColon) {
+      result.push(result.pop()+token);
+    } else {
+      result.push(token);
+    }
+
+    if(token.match(/\"/)) {
+      isSemiColon = !isSemiColon;
+    }
+  }
+
+  return result;
+}
+
+function buildJSON(headers, rows) {
+  return rows.map(row => {
+    const item = {};
+    for(let i=0;i<headers.length;i++) {
+      item[headers[i]] = row[i];
+    }
+    return item;
+  });
+}
+
+onmessage = e => {
+  const [headers, ...rows]= e.data.split('\n').filter(row => row.length > 0).map(processRowToArray);
+  return postMessage(buildJSON(headers, rows));
+};
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function readFile(file) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const reader = new FileReader();
     reader.addEventListener('load', function (e) {
       resolve(e.target.result);
     });
 
     reader.readAsBinaryString(file);
+  });
+}
+
+function getJsonFromCSVString(string) {
+  const worker = new Worker('csv2json.js');
+  worker.postMessage(string);
+  return new Promise(resolve => {
+    worker.onmessage = e => {
+      resolve(e.data);
+    }
   });
 }
 
@@ -17,7 +27,7 @@ async function csvSubmit(e) {
   }
   const [ file ] = files;
   const fileString = await readFile(file);
-  console.log(fileString);
+  console.log(await getJsonFromCSVString(fileString));
 }
 
 document.getElementById('csvform').addEventListener('submit', csvSubmit, false);
