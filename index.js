e126807e5d7d518b177ac67e465b35d2dc7fa38a
Abhimanyu Singh <superhit0@gmail.com>
2020-05-07 11:20:29 +0530

    Adding Download Option

--- index.js
+++ index.js
@@ -48,5 +48,22 @@ function copyResult() {
   resultTextArea.blur();
 }
 
+function downloadFile(filename, text) {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+function downloadResult() {
+  const text = resultTextArea.textContent;
+  downloadFile('jsonResult.json', text);
+}
+
 document.getElementById('csvform').addEventListener('submit', csvSubmit, false);
 document.getElementById('copy-result').addEventListener('click', copyResult);
+document.getElementById('download-result').addEventListener('click', downloadResult);
